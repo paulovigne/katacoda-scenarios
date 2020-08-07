1fd2310cb38c2686f09f8c19706cd6ca7669f117
Paulo Vigne <paulo@necronomicon.local>
2020-08-07 09:59:25 -0300

    k8s

--- k8s-basico/step1-foreground.sh
+++ k8s-basico/step1-foreground.sh
@@ -1,5 +1,15 @@
 echo "Aguardando o Deploy do Cluster"
 
+result=1
+while [ $result -ne 0 ]
+    do
+        kubectl get ns ingress-nginx
+        if [ $? -eq 0 ]
+            then
+                result="0"
+            fi
+    done
+
 kubectl -n ingress-nginx rollout status deployment ingress-nginx-controller
 
 echo "Tudo pronto!"
