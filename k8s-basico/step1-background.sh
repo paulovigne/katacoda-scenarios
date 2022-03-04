ab4753717b0fff6db7a71da3aae467a89cd2e849
paulovigne <57726813+paulovigne@users.noreply.github.com>
2022-03-04 16:05:44 -0300

    Update step1-background.sh

--- k8s-basico/step1-background.sh
+++ k8s-basico/step1-background.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
-sleep 10
+echo -n "Verificando o estado do Cluster..."
+
+while [ $(kubectl get node | grep Ready | wc -l) -ne 2 ]
+    do
+        sleep 1
+        echo -n "."
+    done
+
+echo
+echo "Verificando o Deploy do Ingress Controller"
 
 kubectl apply -f ./manifestos/install-ingress.yaml
+
+result=1
+while [ $result -ne 0 ]
+    do
+        sleep 10
+        kubectl get ns ingress-nginx
+        if [ $? -eq 0 ]
+            then
+                result=0
+            fi
+    done
+
+kubectl -n ingress-nginx rollout status deployment ingress-nginx-controller
+
+echo "Tudo pronto!"
+sleep 1
+clear
