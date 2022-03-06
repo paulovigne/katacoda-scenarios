5787a2e307486cf1083525a825749196d26e6c4b
Paulo Vigne <paulo.vigne@gmail.com>
2022-03-06 16:59:18 -0300

    docker basico

---
+++ docker-basico/init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -n "Verificando o estado o Docker..."
+
+result=1
+while [ $result -ne 0 ]
+    do
+        sleep 10
+        docker info
+        if [ $? -eq 0 ]
+            then
+                result=0
+            fi
+    done
+
+docker images -a | awk '{print $3}' | grep -v IMAGE | xargs docker rmi -f
+
+echo "Tudo pronto!"
+sleep 1
+clear
+
+
+
+
+
+
--- docker-basico/intro.md
+++ docker-basico/intro.md
@@ -1,6 +1,6 @@
 ### Bem ao Treinamento de Docker
 
-Neste treinamento aprenderemos a utilizar funções básicad do Docker, como por exemplo:
+Neste treinamento aprenderemos a utilizar funções básicas do Docker, como por exemplo:
 
 * Gerenciar, Baixar e Executar Imagens
 * Expor portas e Utilizar Variáveis
