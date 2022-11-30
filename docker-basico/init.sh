27a6288686d52c2964adadd46ae0ab0d10be8c23
paulovigne <57726813+paulovigne@users.noreply.github.com>
2022-11-30 14:40:48 -0300

    Update init.sh

--- docker-basico/init.sh
+++ docker-basico/init.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-echo -n "Verificando o estado o Docker..."
-
-result=1
-while [ $result -ne 0 ]
-    do
-        sleep 10
-        docker info
-        if [ $? -eq 0 ]
-            then
-                result=0
-            fi
-    done
-
-#docker images -a | awk '{print $3}' | grep -v IMAGE | xargs docker rmi -f
 apt -y install net-tools jq
 
 echo "Tudo pronto!"
